70892ae642ae0257940c30bdf8cc8c382ea8a3a9
sofhiautaka <sofhia.utaka@sptech.school>
2024-03-05 00:24:52 -0300

    style: atualizada seção 'Quem somos/Nossa equipe'

--- site-institucional/js/main.js
+++ site-institucional/js/main.js
@@ -67,6 +67,33 @@
         return false;
     });
 
+       // Team carousel
+       $(".team-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        center: false,
+        dots: false,
+        loop: true,
+        margin: 50,
+        nav : true,
+        navText : [
+            '<i class="bi bi-arrow-left"></i>',
+            '<i class="bi bi-arrow-right"></i>'
+        ],
+        responsiveClass: true,
+        responsive: {
+            0:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            992:{
+                items:3
+            }
+        }
+    });
+
 
     // Testimonials carousel
     $(".testimonial-carousel").owlCarousel({
@@ -106,4 +133,5 @@
     });
     
 })(jQuery);
+
 
